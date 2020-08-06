a161106c9e3fa4b7138f0e15bf4e532867ea9aa3
blankafogarasi <64541074+blankafogarasi@users.noreply.github.com>
2020-08-06 17:40:08 +0200

    Add new item and remove an item added to the app, but further optimalization needed

--- ToDo/list-tasks.ts
+++
@@ -1,8 +0,0 @@
-import fs from 'fs';
-
-export default class ListOfTasks {
-
-    listTasks () {
-        console.log(fs.readFileSync('list-tasks.txt', 'utf8'))
-    }
-}
--- ToDo/toDo.ts
+++ ToDo/toDo.ts
@@ -1,13 +1,12 @@
 import fs from 'fs';
-import ListOfTasks from './list-tasks';
 //import Plant from './Plant';
+
+let listTasksText = fs.readFileSync('list-tasks.txt', 'utf8');
+let listTasks: string [] = [];
+listTasks = listTasksText.split('\n');
 
 function printUsage () {
     console.log(fs.readFileSync('usage.txt', 'utf8'))
-}
-
-function listTasks () {
-    console.log(fs.readFileSync('list-tasks.txt', 'utf8'))
 }
 
 if (process.argv.length == 2) {
@@ -15,5 +14,38 @@ if (process.argv.length == 2) {
 }
 
 if (process.argv[2] == '-l') {
-    listTasks()
+    if (listTasksText.length === 0) {
+        console.log('No todos for today! :)');
+    } else{
+    console.log(listTasks
+    .map((thing, index) => `${index + 1} ${thing.toString()} `)
+    .join('\n'));
+    }
 }
+/*if (process.argv[2] == '-l') {
+    for (let i = 0; i < listTasks.length; i++ ) {
+        console.log(`${i +1} ${listTasks[i]}`);
+    }
+} */
+
+if (process.argv[2] == '-a') {
+    fs.appendFile('list-tasks.txt', process.argv[3], 'utf8', function (err) {
+        if (err) return console.log(err);
+    });
+}
+/*let newToDo = '';
+if (process.argv[2] == '-a' && process.argv[3] == newToDo) {
+    listTasks.push(newToDo);
+}
+*/
+
+if (process.argv[2] == '-r') {
+    for(var i = 0; i <= listTasks.length; i++) {
+    if (listTasks[i] === process.argv[3]) {
+            listTasks.splice(i, 1);
+        }
+    }
+    let content = listTasks.join('/n');
+    fs.writeFileSync('list-tasks.txt', content);   
+}
+
